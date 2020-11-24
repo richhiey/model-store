e644d598c42f42ee8013d1eeb5b7aa7a1378cee1
Richhiey <richhiey.thomas@gmail.com>
2020-11-24 03:53:28 +0100

    Add helper functions to preprocess data into Music Transformer MIDI Event Representation

--- models/midi_task_learner.py
+++ models/midi_task_learner.py
@@ -2,10 +2,17 @@
 
 import tensorflow as tf
 from helpers.layers import TransformerEncoder, TransformerDecoder
+
 class MIDITransformer(tf.keras.Model):
 
-	def __init__(self):
-		self.midi_encoder = TransformerEncoder()
+	def __init__(self, config_path, model_path):
+		self.config_path = config_path
+		self.model_path = model_path
+
+		with open('data.json') as json_file: 
+    		self.configs = json.load(self.config_path)
+
+		self.midi_encoder = TransformerEncoder(self.configs['encoder'])
 		self.midi_decoder = TransformerDecoder()
 
 	def __create_model__(self):
